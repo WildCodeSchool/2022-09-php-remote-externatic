a40fe22922f4b214681a5e4fb6e84909088868a7
jfm <jeanfrancois.morin@wildcodeschool.com>
2023-11-19 14:31:10 +0100

    remove nelmio/cors-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -23,6 +23,5 @@ return [
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Exercise\HTMLPurifierBundle\ExerciseHTMLPurifierBundle::class => ['all' => true],
-    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\UX\Typed\TypedBundle::class => ['all' => true],
 ];
